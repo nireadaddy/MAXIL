06b77f321ab068e0221d21f735473e552434cb1b
nireadaddy <84140194+nireadaddy@users.noreply.github.com>
2021-06-16 03:16:04 +0700

    update home

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -91,9 +91,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" scale="xl" mb="24px" color="secondary">
-          {t('PancakeSwap')}
+          {t('MAXILSWAP')}
         </Heading>
-        <Text>{t('The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
+        <Text>{t('Yield farm on Binance Smart Chain.')}</Text>
       </Hero>
       <div>
         <Cards>
